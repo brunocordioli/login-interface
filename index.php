7fccbeaaefbe04d2f24f593602ee94025ecabb92
Bruno Cordioli <bcordiolimachado7@gmail.com>
2019-07-04 14:52:12 -0300

    Added Login Interface

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-bet">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My App</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="wrapper">
         <div class="wrapped-div">
-            <h1>My App</h1>
-            <ul>
-                <li><a href="add_user.php">Add User</a></li>
-                <li><a href="list_user.php">List of Users</a></li>
-            </ul>
+            <form method="get" action="login_2.php">
+                <label for="username">Username: <br>
+                    <input type="text" name="username"> <br>
+                </label>
+                <label for="password">Password: <br>
+                    <input type="password" name="password"> <br> <br>
+                </label>
+                <button type="submit">Enter</button> <br> <br>
+                <p style="margin-right: 1.5vw;"><?php $msg = $_GET['msg']; echo $msg;?></p>
+            </form>
         </div>
     </div>
 </body>
---
+++ login_2.php
@@ -0,0 +1,31 @@
+<?php
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+    $con = mysqli_connect("localhost", "root", "gtt123", "curso-db");
+    echo 'ewpowepovwe';
+    $sql = "SELECT username, password, status from finalproject";
+    echo 'ewpowepovwe';
+    $result = $con-> query($sql);
+    echo 'ewpowepovwe';
+    if ($result-> num_rows > 0){
+        while($row = $result-> fetch_assoc()){
+            $row3 = "status";
+            $row4 = "username";
+            $row5 = "password";
+            if ($row[$row4] == $username && $row[$row5] == $password && $row[$row3] == "A"){
+                header("Location: nav.php");
+                $redirect = false;
+                break;
+            } else {
+                $redirect = true;
+                $msg = "User not found";
+            }
+        }
+    }
+    if ($redirect){
+        header("Location: index.php?msg=$msg");
+    }
+    $con-> close();
+?>
