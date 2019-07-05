134d02d02d7bdd12366ad67ea7316c1dcd1916d7
Bruno Cordioli <bcordiolimachado7@gmail.com>
2019-07-05 12:32:07 -0300

    Nav Bar for all the Interfaces

--- nav.php
+++ nav.php
@@ -1,3 +1,8 @@
+<!--
+ Projected and made by Bruno Cordioli Machado
+ Please access my website "brunocordioli.tk" to know more about me!
+ --> 
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,13 +13,16 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <div class="wrapper">
-        <div class="wrapped-div">
-            <h1>my application</h1>
-            <ul>
-                <li><a href="add_user.php">Add User</a></li>
-                <li><a href="list_user.php">List of Users</a></li>
-            </ul>
+    <div class="nav-box">
+        <div class="nav-boxed">
+            <h1>MY APP</h1>
+            <div id="list">
+                <ul>
+                    <li><a class="link" href="add_user.php">ADD USER</a></li>
+                    <li><a class="link" href="list_user.php">LIST OF USERS</a></li>
+                    <li><a class="link" href="index.php">LOGOUT</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </body>
