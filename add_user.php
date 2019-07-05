2940f883eb9546c3806aa8018735a3c8b13a1e52
Bruno Cordioli <bcordiolimachado7@gmail.com>
2019-07-05 12:25:47 -0300

    Add User Interface

--- add_user.php
+++ add_user.php
@@ -1,32 +1,32 @@
+<!--
+ Projected and made by Bruno Cordioli Machado
+ Please access my website "brunocordioli.tk" to know more about me!
+ --> 
+
 <!DOCTYPE html>
-<html lang="pt-bet">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My App</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+<html lang="pt-br">
 <body>
-    <div class="wrapper">
-        <div class="wrapped-div">
-            <form method="post" action="add_user_2.php">
-                <a id="voltar" href="nav.php">Voltar</a><br><br> <br>
-                <label for="name">Nome: <br>
-                    <input type="text" name="name"> <br>
-                </label>
-                <label for="email">Email: <br>
-                    <input type="text" name="email"> <br>
-                </label>
-                <label for="username">Username: <br>
-                    <input type="text" name="username"> <br>
-                </label>
-                <label for="password">Password: <br>
-                    <input type="password" name="password"> <br> <br>
-                </label>
-                <button type="submit">Submit</button> <br> <br>
-                <p style="margin-right: 1.5vw;"><?php $msg = $_GET['msg']; echo $msg;?></p>
-            </form>
+    <?php include_once('nav.php')?>
+    <div id="add-content">
+        <div class="wrapper">
+            <div class="wrapped-div">
+                <form method="post" action="add_user_2.php">
+                    <label for="name"><p class="text">Nome:</p>
+                        <input type="text" name="name"> <br>
+                    </label>
+                    <label for="email"><p class="text">Email:</p>
+                        <input type="text" name="email"> <br>
+                    </label>
+                    <label for="username"><p class="text">Username:</p>
+                        <input type="text" name="username"> <br>
+                    </label>
+                    <label for="password"><p class="text">Password:</p>
+                        <input type="password" name="password"> <br> <br>
+                    </label>
+                    <button type="submit">Submit</button> <br> <br>
+                    <p class="response"><?php $msg = $_GET['msg']; echo $msg;?></p>
+                </form>
+            </div>
         </div>
     </div>
 </body>
